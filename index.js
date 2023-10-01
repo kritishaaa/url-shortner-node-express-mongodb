f3392f2cca381b4d4e89bce956c3d23777f1b5fc
kritishaaa <kritishamoktan530@gmail.com>
2023-10-01 22:07:01 +0545

    css left

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express=require('express');
 const Port =2000;
 const app= express();
 
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res)=>{
+res.render('index');
+})
+
 
 const startServer=()=>{
         try {
